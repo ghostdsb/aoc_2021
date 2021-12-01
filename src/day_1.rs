2f8fb023af0c0a958b00e820d2006390841945c7
ghostdsb <dibyanshu.bhoi@gmail.com>
2021-12-01 18:56:47 +0530

    feat: day_1

---
+++ src/day_1.rs
@@ -0,0 +1,51 @@
+#[allow(unused)]
+pub mod sol{
+    
+    use std::fs;
+
+    pub fn aoc(part: u8) -> u64 {
+        let input_path = format!("./input/day{}.txt", 1);
+        match (fs::read_to_string(&input_path), part){
+            (Ok(content), 1) => {
+                part1(content)
+            },
+            (Ok(content), 2) => {
+                part2(content)
+            },
+            (_, _) => {
+                println!("something wrong");
+                0
+            }
+        }
+    }
+    
+    fn part1( content: String) -> u64 {
+        let mut ans = 0;
+        let depths = content
+        .split('\n')
+        .map(|d| d.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>();
+        
+        for i in 1..depths.len(){
+            if depths[i] > depths[i-1] {
+                ans += 1;
+            } 
+        } 
+        ans
+    }
+    
+    fn part2( content: String) -> u64 {
+        let mut ans = 0;     
+        let depths = content
+        .split('\n')
+        .map(|d| d.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>();
+        
+        for i in 1..depths.len()-2{
+            if depths[i+2] > depths[i-1] {
+                ans += 1;
+            } 
+        } 
+        ans
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod day_1;
+
+fn main() {
+    println!("Hello, AOC!");
+
+    let ans1 = day_1::sol::aoc(1);
+    let ans2 = day_1::sol::aoc(2);
+    println!("day1: part1: {}", ans1);
+    println!("day1: part2: {}", ans2);
+}
